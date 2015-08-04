5797c3b3624c61bcf35f8ca08d6b6aed3fc072a1
Matt Colyer <matt@colyer.name>
2015-08-04 07:15:34 -0700

    Add a sunlight signal

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 
 gem 'posix-mqueue'
+gem 'RubySunrise'
---
+++ signals/sunlight.rb
@@ -0,0 +1,36 @@
+require './signals/base'
+require 'solareventcalculator'
+
+class SunlightSignal < BaseSignal
+  def initialize(name, config)
+    @name = name
+    @config = config
+    $0 = "signald: #{@name}"
+  end
+
+  def watch
+    prev_state = nil
+    state = nil
+
+    while true do
+      date = Date.today
+      calc = SolarEventCalculator.new(date, BigDecimal.new("37.482778"), BigDecimal.new("-122.236111"))
+      sunrise = calc.compute_official_sunrise("America/Los_Angeles")
+      sunset = calc.compute_official_sunset("America/Los_Angeles")
+      now = DateTime.now
+
+      if now > sunrise && now < sunset
+        state = true
+      elsif state != false
+        state = false
+      end
+
+      if prev_state != state
+        send_signal(state)
+        prev_state = state
+      end
+
+      sleep 60
+    end
+  end
+end
