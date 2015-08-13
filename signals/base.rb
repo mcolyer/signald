aecc5381abddd7e41669e252c646350a5e929385
Matt Colyer <matt@colyer.name>
2015-08-13 07:05:05 -0700

    Refactor things a bit

--- signals/base.rb
+++ signals/base.rb
@@ -1,11 +1,12 @@
 class BaseSignal
   def send_signal(bool)
-    queue = POSIX_MQ.new("/signald", :w)
-    queue.nonblock = true
-    begin
-      queue.send "id=#{@name} value=#{bool}"
-    rescue Errno::EAGAIN
-      $stderr.puts "Queue was full"
+    POSIX_MQ.open("/signald", :w) do |queue|
+      queue.nonblock = true
+      begin
+        queue.send "id=#{@name} value=#{bool}"
+      rescue Errno::EAGAIN
+        $stderr.puts "Queue was full"
+      end
     end
   end
 end
